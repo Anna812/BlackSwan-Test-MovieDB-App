1f3d4e14fabd76da16b2f0923b8127dd0460c5d4
Anna812 <88menyus@gmail.com>
2017-02-27 23:14:45 +0100

    now response.body is not null, and we can set up the adapter

--- app/src/main/java/com/annadalnoki/androidtest/MainActivity.java
+++ app/src/main/java/com/annadalnoki/androidtest/MainActivity.java
@@ -2,30 +2,39 @@ package com.annadalnoki.androidtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import com.annadalnoki.androidtest.models.LoadPopularMoviesResponse;
+import com.annadalnoki.androidtest.models.Movie;
 import com.annadalnoki.androidtest.network.MovieDbManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends Activity {
+
     RecyclerView recyclerView;
+    List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.movielist);
 
+        recyclerView = (RecyclerView) findViewById(R.id.movielist);
 
         MovieDbManager.getInstance().loadPopularMovies(1, new Callback<LoadPopularMoviesResponse>() {
             @Override
             public void onResponse(Call<LoadPopularMoviesResponse> call, Response<LoadPopularMoviesResponse> response) {
-                Toast.makeText(MainActivity.this, "Successful load", Toast.LENGTH_SHORT).show();
+                LoadPopularMoviesResponse loadedMovies = response.body();
+                movies = loadedMovies.getMovies();
+                setupAdapter();
             }
 
             @Override
@@ -33,5 +42,11 @@ public class MainActivity extends Activity {
                 Toast.makeText(MainActivity.this, "Error: " + t.toString(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void setupAdapter() {
+        MovieListAdapter movieListAdapter = new MovieListAdapter(this, movies);
+        recyclerView.setAdapter(movieListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
