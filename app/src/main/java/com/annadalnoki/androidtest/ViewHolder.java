7e23e3e3cd56596c17a6f9021d0013cddc3d2732
Anna812 <88menyus@gmail.com>
2017-02-27 22:27:00 +0100

    create ViewHolder class for Adapter

---
+++ app/src/main/java/com/annadalnoki/androidtest/ViewHolder.java
@@ -0,0 +1,22 @@
+package com.annadalnoki.androidtest;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Anna on 17/02/27.
+ */
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView title, genre, description, info;
+
+    public ViewHolder(View itemView) {
+        super(itemView);
+        title = (TextView) itemView.findViewById(R.id.title);
+        genre = (TextView) itemView.findViewById(R.id.genre);
+        description = (TextView) itemView.findViewById(R.id.description);
+        info = (TextView) itemView.findViewById(R.id.info);
+    }
+}
