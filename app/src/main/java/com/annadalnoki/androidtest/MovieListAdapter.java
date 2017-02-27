5d3ef3355c2b835918c18f258b61ed480b5b1f8f
Anna812 <88menyus@gmail.com>
2017-02-27 22:27:49 +0100

    extend MovieListAdapter with two fiealds and an all args constructor

--- app/src/main/java/com/annadalnoki/androidtest/MovieListAdapter.java
+++ app/src/main/java/com/annadalnoki/androidtest/MovieListAdapter.java
@@ -1,22 +1,35 @@
 package com.annadalnoki.androidtest;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
+
+import com.annadalnoki.androidtest.models.LoadPopularMoviesResponse;
+import com.annadalnoki.androidtest.models.Movie;
+
+import java.util.List;
 
 /**
  * Created by Anna on 17/02/27.
  */
 
-public class MovieListAdapter extends RecyclerView.Adapter {
+public class MovieListAdapter extends RecyclerView.Adapter<ViewHolder> {
 
+    private List<Movie> movies;
+    private Context context;
+
+    public MovieListAdapter(Context context, LoadPopularMoviesResponse loadPopularMoviesResponse) {
+        this.context = context;
+        movies = loadPopularMoviesResponse.getMovies();
+    }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
 
     }
 
