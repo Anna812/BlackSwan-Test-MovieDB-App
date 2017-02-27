eafea9f02cfe2509094299d95519b843ec5a0abc
Anna812 <88menyus@gmail.com>
2017-02-27 22:38:19 +0100

    delete info field as it does not have to be populated

--- app/src/main/java/com/annadalnoki/androidtest/ViewHolder.java
+++ app/src/main/java/com/annadalnoki/androidtest/ViewHolder.java
@@ -10,13 +10,12 @@ import android.widget.TextView;
 
 public class ViewHolder extends RecyclerView.ViewHolder {
 
-    public TextView title, genre, description, info;
+    public TextView title, genre, description;
 
     public ViewHolder(View itemView) {
         super(itemView);
         title = (TextView) itemView.findViewById(R.id.title);
         genre = (TextView) itemView.findViewById(R.id.genre);
         description = (TextView) itemView.findViewById(R.id.description);
-        info = (TextView) itemView.findViewById(R.id.info);
     }
 }
