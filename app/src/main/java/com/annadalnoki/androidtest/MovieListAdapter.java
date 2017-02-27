b80ffdb09e82e62bd20b2fe99d2948b0bd9f3bc6
Anna812 <88menyus@gmail.com>
2017-02-27 20:17:05 +0100

    create adapter to RecyclerView

---
+++ app/src/main/java/com/annadalnoki/androidtest/MovieListAdapter.java
@@ -0,0 +1,27 @@
+package com.annadalnoki.androidtest;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Anna on 17/02/27.
+ */
+
+public class MovieListAdapter extends RecyclerView.Adapter {
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
