a0a0fb65ae094e2a7b94626ec07784cb0be003a4
Anna812 <88menyus@gmail.com>
2017-02-28 15:53:44 +0100

    implement onClick method in ViewHolder

--- app/src/main/java/com/annadalnoki/androidtest/adapter/ViewHolder.java
+++ app/src/main/java/com/annadalnoki/androidtest/adapter/ViewHolder.java
@@ -1,11 +1,14 @@
 package com.annadalnoki.androidtest.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.annadalnoki.androidtest.R;
+import com.annadalnoki.androidtest.activity.DetailedPageActivity;
 
 /**
  * Created by Anna on 17/02/27.
@@ -15,9 +18,12 @@ public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickL
 
     public TextView title, genre, description, rating, date;
     public ImageView image;
+    Context context;
 
-    public ViewHolder(View itemView) {
+    public ViewHolder(View itemView, Context context) {
         super(itemView);
+        this.context = context;
+
         image = (ImageView) itemView.findViewById(R.id.image);
         title = (TextView) itemView.findViewById(R.id.title);
         genre = (TextView) itemView.findViewById(R.id.genre);
@@ -29,6 +35,8 @@ public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickL
     @Override
     public void onClick(View view) {
         int position = getAdapterPosition();
-
+        Intent i = new Intent(context, DetailedPageActivity.class);
+        i.putExtra("position", position);
+        context.startActivity(i);
     }
 }
